0708de9dcd3501cdac1430f0247106e3c459898c
Khadiza Sultana Chowdhury Rimi <141613050+Rimi-03@users.noreply.github.com>
2024-10-08 12:59:06 +0600

    Add files via upload

--- LinkClick/client.py
+++ LinkClick/client.py
@@ -1,7 +1,7 @@
 import socket
 import threading
 import tkinter as tk
-from tkinter import scrolledtext, messagebox
+from tkinter import scrolledtext, messagebox, Listbox, Scrollbar
 
 HOST = '127.0.0.1'
 PORT = 1234
@@ -15,41 +15,64 @@ FONT = ("Helvetica", 14)
 BUTTON_FONT = ("Helvetica", 13, "bold")
 SMALL_FONT = ("Helvetica", 11)
 HEADER_FONT = ("Helvetica", 16, "bold")
+RED = "#FF0000"
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-is_connected = False  
+is_connected = False
+
+# Dictionary to store private chat windows
+private_chat_windows = {}
+blocked_users = set()
 
 def add_message(message):
     message_box.config(state=tk.NORMAL)
-    message_box.insert(tk.END, message + '\n')
+
+    if "[Error]" in message or "[Server]" in message:
+        message_box.insert(tk.END, message + '\n', 'error')
+    else:
+        message_box.insert(tk.END, message + '\n')
+
     message_box.config(state=tk.DISABLED)
+    message_box.see(tk.END)
+
+def update_online_clients(clients_list):
+    online_users_box.config(state=tk.NORMAL)
+    online_users_box.delete(1.0, tk.END)
+    for client in clients_list:
+        online_users_box.insert(tk.END, f"->{client}\n")
+    online_users_box.config(state=tk.DISABLED)
 
 def connect():
     global client, is_connected
+    username = username_textbox.get().strip()
+
+    if username == "":
+        messagebox.showerror("Invalid username", "Username cannot be empty!")
+        username_textbox.focus_set()
+        return
+    
     if is_connected:
         add_message("[Client] Already connected to the server.")
         return
 
     try:
         client.connect((HOST, PORT))
-        add_message("[Server] Successfully connected to the server")
+        add_message("[Server] Connected")
         is_connected = True
     except:
         add_message(f"[Error] Connection to server {HOST}:{PORT} failed.")
         return
-
-    username = username_textbox.get()
-    if username != '':
-        client.sendall(username.encode())
-    else:
-        messagebox.showerror("Invalid username", "Username cannot be empty!")
-
-    threading.Thread(target=listen_for_messages_from_server, args=(client,)).start()
+    
+    client.sendall(username.encode())
+    threading.Thread(target=listen_for_messages_from_server, args=(client,), daemon=True).start()
 
     username_textbox.config(state=tk.DISABLED)
     username_button.config(state=tk.DISABLED)
+    message_button.config(state=tk.NORMAL)
     reconnect_button.config(state=tk.NORMAL)
     logout_button.config(state=tk.NORMAL)
+    private_chat_button.config(state=tk.NORMAL)
+
 
 def reconnect():
     global client, is_connected
@@ -60,7 +83,7 @@ def reconnect():
     try:
         client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client.connect((HOST, PORT))
-        add_message("[Server] Reconnected to the server")
+        add_message("[Server] Reconnected")
         is_connected = True
 
         username = username_textbox.get()
@@ -69,16 +92,17 @@ def reconnect():
 
         threading.Thread(target=listen_for_messages_from_server, args=(client,), daemon=True).start()
     except:
-        add_message("[Error] Reconnection failed. Server may not be running.")
+        add_message("[Error] Reconnection failed. Server is not running.")
 
 def send_message():
     message = message_textbox.get()
+
     if message != '':
         try:
             client.sendall(message.encode())
             message_textbox.delete(0, len(message))
         except:
-            messagebox.showerror("Error", "Failed to send message. Server might be down. Or you might forgot to click 'Reconnect'.")
+            messagebox.showerror("Error", "Failed to send message due to server outage. Click 'Reconnect' or try again later.")
     else:
         messagebox.showerror("Empty message", "Message cannot be empty")
 
@@ -87,17 +111,38 @@ def listen_for_messages_from_server(client):
     while is_connected:
         try:
             message = client.recv(2048).decode('utf-8')
-            if message != '':
+
+            if message.startswith("USERS~"):
+                users = message.replace("USERS~", "").split(", ")
+                update_online_clients(users)
+
+            elif message.startswith("OPEN_PRIVATE_CHAT~"):
+                recipient = message.split("~")[1]
+                open_private_chat(recipient)
+
+            elif message.startswith("[Private]"):
+                handle_private_message(message)
+
+            elif message.startswith("LOGOUT_PRIVATE~"):
+                recipient = message.split("~")[1]
+                if recipient in private_chat_windows:
+                    private_chat_windows[recipient].add_private_message(f"[Server] {recipient} has logged out.")
+
+            elif "[Server]" in message and "has blocked you" in message:
+                handle_private_message(message)  # This will notify the user they have been blocked
+
+            elif message != '':
                 username = message.split("~")[0]
                 content = message.split("~")[1]
                 add_message(f"[{username}] {content}")
+
         except ConnectionResetError:
-            add_message("[Server] Connection lost. Please restart the server and click 'Reconnect'.")
+            add_message("[Server] Connection lost. Click 'Reconnect' to connect to the server.")
             is_connected = False
             break
         except socket.error as e:
             if e.errno == 10053:
-                add_message("[Error] Connection aborted by your host machine.")
+                add_message("[Server] You left the chat.")
                 is_connected = False
                 break
             else:
@@ -109,24 +154,179 @@ def listen_for_messages_from_server(client):
             is_connected = False
             break
 
+
+class PrivateChatWindow:
+    def __init__(self, recipient):
+        self.recipient = recipient
+        self.root = tk.Toplevel()
+        self.root.title(f"Private Chat with {recipient}")
+        self.root.geometry("400x500")
+        self.root.configure(bg=DARK_GREY)
+        self.root.resizable(False, False)
+
+        # For displaying messages
+        self.middle_frame = tk.Frame(self.root, bg=DARK_GREY)
+        self.middle_frame.pack(fill="both", expand=True, padx=10, pady=(10, 0))
+
+        self.chat_box = scrolledtext.ScrolledText(self.middle_frame, font=SMALL_FONT, bg=SOFT_WHITE, fg=DARK_GREY, wrap=tk.WORD)
+        self.chat_box.config(state=tk.DISABLED)
+        self.chat_box.pack(fill="both", expand=True)
+
+        # For message input
+        self.bottom_frame = tk.Frame(self.root, bg=DARK_GREY)
+        self.bottom_frame.pack(fill="x", padx=10, pady=(0, 10))
+
+        self.message_textbox = tk.Entry(self.bottom_frame, font=FONT, bg=LIGHT_GREY, fg=DARK_GREY)
+        self.message_textbox.pack(side=tk.LEFT, fill="x", expand=True, padx=(0, 10))
+
+        self.send_button = tk.Button(self.bottom_frame, text="Send", font=BUTTON_FONT, bg=OCEAN_BLUE, fg=WHITE, command=self.send_private_message)
+        self.send_button.pack(side=tk.RIGHT)
+        self.message_textbox.bind('<Return>', lambda event: self.send_private_message())
+        
+        self.block_button = tk.Button(self.bottom_frame, text="Block", font=BUTTON_FONT, bg=RED, fg=WHITE, command=self.block_user)
+        self.block_button.pack(side=tk.RIGHT, padx=10)
+
+        if self.recipient in blocked_users:
+            self.send_button.config(state=tk.DISABLED)
+
+        self.root.protocol("WM_DELETE_WINDOW", self.on_close)
+
+    def send_private_message(self):
+        if self.recipient in blocked_users:
+            return
+        
+        message = self.message_textbox.get()
+        if message != '':
+            try:
+                formatted_message = f"PRIVATE~{self.recipient}~{message}"
+                client.sendall(formatted_message.encode())  # Send private message to server
+                self.add_private_message(f"[Me] {message}")  # Display in the chat window
+                self.message_textbox.delete(0, tk.END)
+            except:
+                messagebox.showerror("Error", "Failed to send private message.")
+        else:
+            messagebox.showerror("Empty message", "Message cannot be empty")
+
+    def add_private_message(self, message):
+        self.chat_box.config(state=tk.NORMAL)
+        self.chat_box.insert(tk.END, message + '\n')
+        self.chat_box.config(state=tk.DISABLED)
+        self.chat_box.see(tk.END)
+
+    def block_user(self):
+        """Add the recipient to the blocked users list."""
+        blocked_users.add(self.recipient)
+        self.add_private_message(f"[Server] You blocked {self.recipient}.")
+        self.block_button.config(state=tk.DISABLED)
+        #self.send_button.config(state=tk.DISABLED)
+
+    def on_close(self):
+        del private_chat_windows[self.recipient]  # Remove window from dictionary
+        self.root.destroy()
+
+def handle_private_message(message):
+    try:
+        print(f"Received private message: {message}")
+        parts = message.split("~")
+
+        if len(parts) != 2:
+            return
+
+        sender = parts[0].replace("[Private] ", "")
+        content = parts[1]
+
+        if sender in blocked_users:
+            add_message(f"[Server] Message from {sender} blocked.")
+            return
+
+        # Check if a private chat window is already open with the sender
+        if sender in private_chat_windows:
+            private_chat_windows[sender].add_private_message(f"[{sender}] {content}")
+        else:
+            # Open window for recipient
+            window = PrivateChatWindow(sender)
+            private_chat_windows[sender] = window
+            window.add_private_message(f"[{sender}] {content}")
+    except Exception as e:
+        add_message(f"[Error] {str(e)}")
+
+def open_private_chat():
+    # Create a new Toplevel window for user selection
+    selection_window = tk.Toplevel(root)
+    selection_window.title("Select User for Private Chat")
+    selection_window.geometry("200x200")
+    selection_window.configure(bg=DARK_GREY)
+
+    frame = tk.Frame(selection_window, bg=DARK_GREY)
+    frame.pack(pady=10)
+
+    scrollbar = Scrollbar(frame)
+    scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+    user_listbox = Listbox(frame, font=SMALL_FONT, bg=LIGHT_GREY, fg=DARK_GREY, yscrollcommand=scrollbar.set)
+    user_listbox.pack(side=tk.LEFT, fill=tk.BOTH)
+
+    scrollbar.config(command=user_listbox.yview)
+
+    # Populate Listbox with online users
+    users = online_users_box.get(1.0, tk.END).strip().split("\n")
+    for user in users:
+        if user and user != f"->{username_textbox.get()}":  # Avoid selecting own username
+            user_listbox.insert(tk.END, user[2:])
+
+    def on_select(event):
+        selected_user = user_listbox.get(user_listbox.curselection())
+        open_private_chat_window(selected_user)
+        selection_window.destroy()
+
+    user_listbox.bind('<<ListboxSelect>>', on_select)
+
+
+def open_private_chat_window(recipient):
+    """Open chat with the selected user."""
+    if recipient in private_chat_windows:
+        if private_chat_windows[recipient].root.winfo_exists():   # Check if window is still open
+            private_chat_windows[recipient].root.lift()
+        else:
+            private_chat_windows[recipient] = PrivateChatWindow(recipient)    # If not create a new one
+    else:
+        window = PrivateChatWindow(recipient)
+        private_chat_windows[recipient] = window  # Store window in dictionary
+
 def logout():
     global is_connected
     if is_connected:
         try:
-            client.sendall("LOGOUT".encode()) 
+            client.sendall("LOGOUT".encode())
             client.close()
             is_connected = False
             add_message("[Client] Disconnected from server.")
             username_textbox.config(state=tk.DISABLED)
             username_button.config(state=tk.DISABLED)
             reconnect_button.config(state=tk.NORMAL)
+
+            online_users_box.config(state=tk.NORMAL)
+            online_users_box.delete(1.0, tk.END)
+            online_users_box.config(state=tk.DISABLED)
         except:
             messagebox.showerror("Error", "Failed to disconnect properly.")
     else:
         messagebox.showinfo("Info", "You are not connected to the server.")
 
+def on_window_close():
+    global is_connected
+    if is_connected:
+        try:
+            client.sendall("LOGOUT".encode())
+            client.close()
+            add_message("[Client] Disconnected from server.")
+        except:
+            messagebox.showerror("Error", "Failed to disconnect properly.")
+    root.destroy()
+
+#GUI
 root = tk.Tk()
-root.geometry("750x650")
+root.geometry("1000x700")
 root.title("LinkClick Chat Client")
 root.configure(bg=DARK_GREY)
 root.resizable(False, False)
@@ -134,8 +334,8 @@ root.resizable(False, False)
 top_frame = tk.Frame(root, bg=DARK_GREY, pady=10)
 top_frame.pack(fill="x", padx=10, pady=10)
 
-header_label = tk.Label(top_frame, text="LinkClick Chat", font=HEADER_FONT, bg=DARK_GREY, fg=OCEAN_BLUE)
-header_label.pack()
+header_label = tk.Label(top_frame, text="Global Chat", font=HEADER_FONT, bg=DARK_GREY, fg=OCEAN_BLUE)
+header_label.pack(pady=(0,5))
 
 username_label = tk.Label(top_frame, text="Enter Username: ", font=FONT, bg=DARK_GREY, fg=SOFT_WHITE)
 username_label.pack(side=tk.LEFT, padx=(10, 0))
@@ -146,29 +346,44 @@ username_textbox.pack(side=tk.LEFT, padx=10)
 username_button = tk.Button(top_frame, text="Join", font=BUTTON_FONT, bg=OCEAN_BLUE, fg=WHITE, command=connect)
 username_button.pack(side=tk.LEFT, padx=10)
 
-reconnect_button = tk.Button(top_frame, text="Reconnect", font=BUTTON_FONT, bg='#FF6347', fg=WHITE, command=reconnect)
-reconnect_button.pack(side=tk.LEFT, padx=5)
+reconnect_button = tk.Button(top_frame, text="Reconnect", font=BUTTON_FONT, bg='#50C878', fg=WHITE, command=reconnect)
+reconnect_button.pack(side=tk.LEFT, padx=10)
 reconnect_button.config(state=tk.DISABLED)
 
 logout_button = tk.Button(top_frame, text="Logout", font=BUTTON_FONT, bg='#FF6347', fg=WHITE, command=logout)
-logout_button.pack(side=tk.LEFT, padx=5)
+logout_button.pack(side=tk.LEFT, padx=10)
 logout_button.config(state=tk.DISABLED)
 
-middle_frame = tk.Frame(root, bg=LIGHT_GREY, padx=10, pady=10)
-middle_frame.pack(fill="both", expand=True, padx=10, pady=10)
+middle_frame = tk.Frame(root, bg=LIGHT_GREY, padx=10 , pady=10)
+middle_frame.pack(fill="both", expand=True, padx=10, pady=10, side=tk.LEFT)
 
 message_box = scrolledtext.ScrolledText(middle_frame, font=SMALL_FONT, bg=SOFT_WHITE, fg=DARK_GREY, wrap=tk.WORD)
 message_box.config(state=tk.DISABLED)
 message_box.pack(fill="both", expand=True)
 
-bottom_frame = tk.Frame(root, bg=DARK_GREY, pady=10)
-bottom_frame.pack(fill="x", padx=10, pady=10)
+message_box.tag_configure('error', foreground=RED)
 
-message_textbox = tk.Entry(bottom_frame, font=FONT, bg=LIGHT_GREY, fg=DARK_GREY, width=55)
-message_textbox.pack(side=tk.LEFT, padx=10)
+message_textbox = tk.Entry(middle_frame, font=FONT, bg=LIGHT_GREY, fg=DARK_GREY, width=55)
+message_textbox.pack(side=tk.LEFT, padx=10, pady=10)
 
-message_button = tk.Button(bottom_frame, text="Send", font=BUTTON_FONT, bg=OCEAN_BLUE, fg=WHITE, command=send_message)
+message_button = tk.Button(middle_frame, text="Send", font=BUTTON_FONT, bg=OCEAN_BLUE, fg=WHITE, command=send_message)
 message_button.pack(side=tk.LEFT, padx=10)
+message_button.config(state=tk.DISABLED)
+
+right_frame = tk.Frame(root, bg=DARK_GREY, padx=10, pady=10)
+right_frame.pack(fill="y", side=tk.RIGHT)
+
+online_label = tk.Label(right_frame, text="Online", font=HEADER_FONT, bg=DARK_GREY, fg=OCEAN_BLUE)
+online_label.pack(pady=5)
+
+online_users_box = tk.Text(right_frame, font=SMALL_FONT, bg=SOFT_WHITE, fg=DARK_GREY, height=30, width=20)
+online_users_box.config(state=tk.DISABLED)
+online_users_box.pack(padx=5, pady=5)
+
+private_chat_button = tk.Button(top_frame, text="Private Chat", font=BUTTON_FONT, bg='#3F51B5', fg=WHITE, command=open_private_chat)
+private_chat_button.pack(side=tk.LEFT, padx=10)
+private_chat_button.config(state=tk.DISABLED)
+
 
 def enter_pressed(event):
     if username_textbox['state'] == 'normal':
@@ -177,6 +392,8 @@ def enter_pressed(event):
         send_message()
 
 root.bind('<Return>', enter_pressed)
+
+root.protocol("WM_DELETE_WINDOW", on_window_close)
 
 def main():
     root.mainloop()
--- LinkClick/server.py
+++ LinkClick/server.py
@@ -6,54 +6,92 @@ from tkinter import messagebox
 HOST = '127.0.0.1'
 PORT = 1234
 LISTENER_LIMIT = 5
-active_clients = []
+active_clients = {}
 server = None
 is_server_running = False
+
+blocked_users = {}
+clients = {}
 
 def listen_for_messages(client, username):
     while True:
         try:
             message = client.recv(2048).decode('utf-8')
+
             if message == 'LOGOUT':
                 remove_client(client, username)
                 break
-            if message != '':
-                final_msg = username + '~' + message
+
+            elif message.startswith('PRIVATE~'):
+                recipient, private_message = message[8:].split('~', 1)
+                send_private_message(username, recipient, private_message)
+
+            elif message.startswith("BLOCK~"):
+                user_to_block = message.split("~")[1]
+                block_user(username, user_to_block)
+
+            elif message != '':
+                final_msg = f"{username}~{message}"
+                add_message_to_chat_activity(final_msg)
                 send_message_to_all(final_msg)
+
             else:
                 print(f"The message sent from client {username} is empty")
         except:
             break
 
+def notify_logout(username, recipient):
+    if recipient in active_clients:
+        logout_message = f"LOGOUT_PRIVATE~{username}"
+        send_message_to_client(active_clients[recipient], logout_message)  # Notify recipient only
+
 def remove_client(client, username):
     global active_clients
-    for u, c in active_clients:
-        if c == client:
-            active_clients.remove((u, c))
-            break
+    client_info = client.getpeername()
+
+    if username in active_clients:
+        # Notify private chat partners before removing the user
+        for user in active_clients.keys():
+            if user != username:
+                notify_logout(username, user)
+        
+        del active_clients[username]
     client.close()
-    prompt_message = f"SERVER~{username} has left the chat."
-    add_message(prompt_message)
+
+    server_log_message = f"Client {client_info[0]}:{client_info[1]} has disconnected."
+    add_message_to_server_log(server_log_message)
+
+    prompt_message = f"Server~{username} has left the chat."
+    add_message_to_chat_activity(prompt_message)
     send_message_to_all(prompt_message)
+    update_connected_clients()
 
 def send_message_to_client(client, message):
     client.sendall(message.encode())
 
 def send_message_to_all(message):
-    for user in active_clients:
-        send_message_to_client(user[1], message)
+    for username, client in active_clients.items():
+        send_message_to_client(client, message)
+
+def send_private_message(sender, recipient, message):
+    if recipient in active_clients:
+        private_msg = f"[Private] {sender}~{message}"
+        send_message_to_client(active_clients[recipient], private_msg)
+        add_message_to_chat_activity(f"[Private] {sender} to {recipient}~{message}")
 
 def client_handler(client):
     while True:
         try:
-            username = client.recv(2048).decode('utf-8')
-            if username != '':
-                active_clients.append((username, client))
-                prompt_message = "SERVER~" + f"{username} added to the chat"
+            username = client.recv(2048).decode('utf-8').strip()
+            clients[client] = username
+            blocked_users[username] = set()
+            if username != '' and username not in active_clients:
+                active_clients[username] = client
+                prompt_message = f"Server~{username} added to the chat"
+                add_message_to_chat_activity(prompt_message)
                 send_message_to_all(prompt_message)
+                update_connected_clients()
                 break
-            else:
-                print("Client username is empty")
         except:
             break
 
@@ -67,7 +105,7 @@ def start_server():
         try:
             server.bind((HOST, PORT))
             print(f"Running the server on {HOST} {PORT}")
-            add_message(f"Server started on {HOST} {PORT}")
+            add_message_to_server_log(f"Server started on {HOST} {PORT}")
         except:
             messagebox.showerror("Error", f"Unable to bind host {HOST} and port {PORT}")
             return
@@ -82,17 +120,18 @@ def stop_server():
     if is_server_running:
         is_server_running = False
 
-        for username, client in active_clients:
+        for username, client in active_clients.items():
             try:
-                client.close() 
+                client.close()
             except:
                 pass
 
-        active_clients.clear()  
+        active_clients.clear()
 
         try:
             server.close()
-            add_message("Server stopped.")
+            add_message_to_server_log("Server stopped.")
+            update_connected_clients()
         except:
             messagebox.showerror("Error", "Failed to stop the server.")
     else:
@@ -103,28 +142,112 @@ def accept_clients():
     while is_server_running:
         try:
             client, address = server.accept()
-            add_message(f"Client {address[0]}:{address[1]} connected.")
+            add_message_to_server_log(f"Client {address[0]}:{address[1]} connected.")
             threading.Thread(target=client_handler, args=(client,), daemon=True).start()
-        except:
+        except Exception as e:
+            print(f"Error accepting clients: {e}")
             break
 
-def add_message(message):
-    message_box.config(state=tk.NORMAL)
-    message_box.insert(tk.END, message + '\n')
-    message_box.config(state=tk.DISABLED)
+def add_message_to_server_log(message):
+    server_log_box.config(state=tk.NORMAL)
+    server_log_box.insert(tk.END, message + '\n')
+    server_log_box.see(tk.END)
+    server_log_box.config(state=tk.DISABLED)
 
+def add_message_to_chat_activity(message):
+    chat_activity_box.config(state=tk.NORMAL)
+    chat_activity_box.insert(tk.END, message + '\n')
+    chat_activity_box.see(tk.END)
+    chat_activity_box.config(state=tk.DISABLED)
+
+def update_connected_clients():
+    connected_clients_box.config(state=tk.NORMAL)
+    connected_clients_box.delete(1.0, tk.END)
+    clients_list = list(active_clients.keys())
+    if clients_list:
+        connected_clients_box.insert(tk.END, ", ".join(clients_list))
+        send_message_to_all(f"USERS~{', '.join(clients_list)}")
+    else:
+        connected_clients_box.insert(tk.END, "")
+    connected_clients_box.see(tk.END)
+    connected_clients_box.config(state=tk.DISABLED)
+
+# Function to broadcast messages to all clients except the blocked ones
+def broadcast(message, sender_client):
+    sender_username = clients[sender_client]
+    for client, username in clients.items():
+        if sender_username in blocked_users.get(username, []):  # Skip blocked users
+            continue
+        try:
+            client.send(message.encode())
+        except Exception as e:
+            print(f"Error sending message to {username}: {e}")
+            remove_client(client)
+
+# Handle private message delivery, skipping blocked recipients
+def send_private_message(sender, recipient, message):
+    recipient_client = None
+    for client, username in clients.items():
+        if username == recipient:
+            recipient_client = client
+            break
+
+    if recipient_client:
+        if sender in blocked_users.get(recipient, []):
+            sender_client = next(client for client, uname in clients.items() if uname == sender)
+            sender_client.send(f"[Server] You have been blocked by {recipient}.".encode())
+            return  # Do not deliver message if the sender is blocked
+        try:
+            recipient_client.send(f"[Private] {sender}~{message}".encode())
+        except Exception as e:
+            print(f"Error sending private message to {recipient}: {e}")
+
+# Add a user to the block list and notify the blocked user
+def block_user(username, user_to_block):
+    if user_to_block in clients.values():  # Ensure the user exists
+        blocked_users[username].add(user_to_block)
+        print(f"{username} blocked {user_to_block}")
+
+        # Notify the blocker that the block was successful
+        blocker_socket = next(client for client, uname in clients.items() if uname == username)
+        blocker_socket.send(f"[Server] You have blocked {user_to_block}.".encode())
+
+        # Notify the blocked user that they have been blocked
+        blocked_user_socket = next(client for client, uname in clients.items() if uname == user_to_block)
+        blocked_user_socket.send(f"[Server] {username} has blocked you. SEND_DISABLED".encode())  # Notify the blocked user
+
+        blocker_socket.send("SEND_DISABLED".encode())  # Disable sender's send button
+        blocked_user_socket.send("SEND_DISABLED".encode())  # Disable blocked user's send button
+
+    else:
+        print(f"User {user_to_block} not found.")
+
+
+# GUI
 root = tk.Tk()
-root.geometry("400x400")
+root.geometry("600x750")
 root.title("Server Control")
 root.resizable(False, False)
 
-message_box = tk.Text(root, height=15, state=tk.DISABLED)
-message_box.pack(pady=20)
+server_log_label = tk.Label(root, text="Server Log:", font=("Helvetica", 14))
+server_log_label.pack(pady=5)
+server_log_box = tk.Text(root, height=10, state=tk.DISABLED)
+server_log_box.pack(pady=10, padx=10)
+
+chat_activity_label = tk.Label(root, text="Chat Activity:", font=("Helvetica", 14))
+chat_activity_label.pack(pady=5)
+chat_activity_box = tk.Text(root, height=10, state=tk.DISABLED)
+chat_activity_box.pack(pady=10, padx=10)
+
+connected_clients_label = tk.Label(root, text="Connected Clients:", font=("Helvetica", 14))
+connected_clients_label.pack(pady=5)
+connected_clients_box = tk.Text(root, height=5, state=tk.DISABLED)
+connected_clients_box.pack(pady=5, padx=10)
 
 start_button = tk.Button(root, text="Start Server", font=("Helvetica", 15), bg='#464EB8', fg='white', command=start_server)
 start_button.pack(pady=5)
 
-stop_button = tk.Button(root, text="Stop Server", font=("Helvetica", 15), bg='#464EB8', fg='white', command=stop_server)
+stop_button = tk.Button(root, text="Stop Server", font=("Helvetica", 15), bg='#FF6347', fg='white', command=stop_server)
 stop_button.pack(pady=5)
 
 def main():
